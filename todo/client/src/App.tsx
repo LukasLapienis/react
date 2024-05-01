656c5db006ee42da19a35f8da2cc9422559e5091
LukasLapienis <lapienislukasp@gmail.com>
2024-05-01 22:48:29 +0300

    update

--- todo/client/src/App.tsx
+++ todo/client/src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <></>;
+  return <>
+  <div className='flex items-center justify-center pt-4'>
+    <button className='duration-1000 border-4 border-gray-300 p-2 rounded-2xl hover:bg-gray-300 hover:scale-110' onClick={() => alert('hi')}>Laba diena</button>
+  </div>
+</>;
 }
 
 export default App;
